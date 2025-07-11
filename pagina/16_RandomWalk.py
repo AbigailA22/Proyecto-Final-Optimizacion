1858bb0450bb1bbb8811f66b32c0a250851bc0a1
AbigailA22 <131839383+AbigailA22@users.noreply.github.com>
2025-07-10 19:16:53 -0600

    Update 16_RandomWalk.py

--- pagina/16_RandomWalk.py
+++ pagina/16_RandomWalk.py
@@ -52,6 +52,7 @@ with col1:
         
         epsilon = st.slider("Selección del valor de epsilon", 0.01, 1.00)    
         Num = st.number_input("Escriba el número deseado de variables", value=2)
+        st.write("Nota: El valor inicial se generará aleatoriamente")
 
         if(option == "Función de Rastrigin"):
             limInf = st.number_input("Escriba el límite inferior", value=-5.12)
